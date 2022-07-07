12e9094deaf181c74415dd2599e8e2a66fc7013d
ChangRongXuan <as840922qq@gmail.com>
2022-07-07 17:43:14 +0800

    background update

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,13 +3,19 @@ import ".//../styles/_background.css";
 
 import bg_left_down from "../imgs/bg-left-down.svg";
 import bg_right_top from "../imgs/bg-right-top.svg";
+import bg_right_star from "../imgs/bg-right-star.svg";
+import bg_left_star from "../imgs/bg-left-star.svg";
+import bg_elf from "../imgs/elf.svg";
 
 
 const Background = () => {
     return (
       <div className="view" style={{position: 'relative', zIndex: -100}}>
-        <img src={bg_left_down} alt=""  class="bg-left" />
-        <img src={bg_right_top} alt=""  class="bg-right" />
+        <img src={bg_left_down} alt=""  class="bg-left-wave" />
+        <img src={bg_right_top} alt=""  class="bg-right-wave" />
+        <img src={bg_left_star} alt=""  class="bg-left-star" />
+        <img src={bg_right_star} alt=""  class="bg-right-star" />
+        <img src={bg_elf} alt=""  class="bg-elf" />
       </div>
     );
 };
